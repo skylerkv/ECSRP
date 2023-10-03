7315ac5cfe67e010907ad4f309548c8c0ffe9509
soliyananegash <soliyananegash@gmail.com>
2023-10-03 10:55:18 -0500

    css updates

--- resourcemapping/src/components/Cards/Cards.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const Cards = () => {
-    return (
-        <h1>cards (this is tbd until i can figure out either database stuff or api stuff idk lol)</h1>
-    );
-}
-export default Cards;
