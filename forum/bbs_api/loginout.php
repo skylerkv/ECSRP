24d0aa9045ed86ffeaf09637ba6ad76050da8284
Zhenyu <127324980+wuliuqi-z@users.noreply.github.com>
2023-12-11 12:37:24 -0600

    Delete forum directory

--- forum/bbs_api/loginout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'base/base.php';
-require_once 'base/account.php';
-
-$account = is_login();
-
-$account->login_out();
-
-json_success("exit", null);
--- forum/bbs_api/userinfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'base/base.php';
-require_once 'base/account.php';
-
-$account = is_login();
-
-$result = ['email' => $account->email];
-json_success("login success", $result);
