1173c824e08644e4c2587fd8c0ef6dc77f9f22d4
Zhenyu <127324980+wuliuqi-z@users.noreply.github.com>
2023-12-11 13:46:00 -0600

    Update allow_publish_article.php

--- Forum/bbs_api/allow_publish_article.php
+++ Forum/bbs_api/allow_publish_article.php
@@ -1,20 +1,20 @@
 <?php
-// 允许某个文章发布
+
 require_once 'base/base.php';
 require_once 'base/account.php';
 require_once 'base/topic.php';
 require_once 'base/article.php';
 
-// 文章ID
+
 $article_id = get("article_id");
 
 $account = is_manager_login();
 
 $article = find_unpublished_article($article_id);
 if (isset($article)) {
-    # 发布文章
+
     allow_publish_article($article);
-    json_success("文章已发布", null);
+    json_success("The article has been published", null);
 } else {
-    json_error("文章不存在，无法发布");
+    json_error("The article does not exist and cannot be published");
 }
